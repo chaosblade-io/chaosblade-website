07d5e51edc88789169020774ba5abc34614c5568
Icesource <gonghuajian.ghj@alibaba-inc.com>
2022-07-28 17:57:55 +0800

    feat: migrate chaosblade-help-zh-cn to website
    Signed-off-by: Icesource <gonghuajian.ghj@alibaba-inc.com>

--- sidebars.js
+++ sidebars.js
@@ -9,6 +9,25 @@ module.exports = {
         "community/PR-guide",
         "community/dev-standard",
         "community/issue-standard",
+      ],
+      "Types of Chaos Experiments": [
+        {
+          "Physical Host":[
+            "experiment-types/host/blade create cpu load",
+            "experiment-types/host/blade create disk burn",
+            "experiment-types/host/blade create disk fill",
+            "experiment-types/host/blade create network delay",
+            "experiment-types/host/blade create network dns",
+            "experiment-types/host/blade create network drop",
+            "experiment-types/host/blade create network loss",
+            "experiment-types/host/blade create network corrupt",
+            "experiment-types/host/blade create network reorder",
+            "experiment-types/host/blade create network duplicate",
+            "experiment-types/host/blade create network occupy",
+            "experiment-types/host/blade create process kill",
+            "experiment-types/host/blade create process stop",
+          ]
+        }
       ]
     }
   }
